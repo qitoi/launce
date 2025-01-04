c62824827223f052c94aea265a09adb6b9ca5aee
qitoi <asacly@gmail.com>
2025-01-04 20:19:49 +0900

    version 1.2.0

--- version.go
+++ version.go
@@ -18,8 +18,8 @@ package launce
 
 const (
 	// Version is the version of launce.
-	Version = "1.1.1"
+	Version = "1.2.0"
 
 	// LocustVersion is the version of locust that launce is compatible with.
-	LocustVersion = "2.32.0"
+	LocustVersion = "2.32.5"
 )
